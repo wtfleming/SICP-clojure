103a363b950bf38b7d1a48c069975fdaa8687a8d
Will Fleming <wfleming77@gmail.com>
2014-02-03 19:41:20 -0800

    start project

---
+++ src/sicp_clojure/ch1.clj
@@ -0,0 +1,20 @@
+(ns sicp.ch1)
+
+;;; Exercise 1.2
+(/
+  (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+  (* 3 (- 6 2) (- 2 7)))
+; -37/150
+
+;;; Exercise 1.3
+(defn square [x] (* x x))
+
+(defn largest-two [a b c]
+  (take 2 (sort > [a b c])))
+
+(defn sum-of-larger-squares
+  "Returns the sum of the squares of the two larger numbers"
+  [a b c]
+  (let [[aa bb] (largest-two a b c)]
+    (+ (square aa) (square bb))))
+
